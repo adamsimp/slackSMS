38f237200878e7a06ef66fda1c90bae479572bbf
Adam Simpson <asimpson@grahamdigital.com>
2017-06-06 14:45:04 -0400

    adding the GD logo

--- src/models/slack.js
+++ src/models/slack.js
@@ -13,7 +13,7 @@ slack.setWebhook(webhookUri);
  * @return {boolean} Does this channel exist already?
  */
 function channelExists(name, listOfChannels) {
-  return (listOfChannels.indexOf(name) > -1);
+    return (listOfChannels.indexOf(name) > -1);
 }
 
 /**
@@ -22,72 +22,64 @@ function channelExists(name, listOfChannels) {
  * @return {boolean} Should we make a new channel or not?
  */
 function validChannel(channelToCheck) {
-  slack.api('channels.list', (error, response) => {
-    const activeChannels = response.channels;
-    this.channels = [];
-    activeChannels.forEach(function add(channel) {
-      this.channels.push(channel.name);
+    slack.api('channels.list', (error, response) => {
+        const activeChannels = response.channels;
+        this.channels = [];
+        activeChannels.forEach(function add(channel) {
+            this.channels.push(channel.name);
+        });
+        /* eslint no-unneeded-ternary: 0 */
+        return channelExists(channelToCheck, this.channels) ? true : false;
     });
-    /* eslint no-unneeded-ternary: 0 */
-    return channelExists(channelToCheck, this.channels) ? true : false;
-  });
 }
 
 module.exports = {
-  sendToSlack: (req, res) => {
-    let usersNumber = req.body.From;
-    // chop off pre-pended + in the number
-    usersNumber = usersNumber.slice(1);
+    sendToSlack: (req, res) => {
+        let usersNumber = req.body.From;
+        // chop off pre-pended + in the number
+        usersNumber = usersNumber.slice(1);
 
-    // continues conversations in same channels, if they exist
-    if (validChannel(`sms${usersNumber}`)) {
-      slack.webhook(
-        {
-          channel: `#sms${usersNumber}`,
-          icon_emoji: ':speech_balloon:',
-          username: process.env.SLACK_BOT_NAME,
-          attachments: [
-            {
-              fallback: `from ${req.body.From}: ${req.body.Body}`,
-              color: '#3D91FC',
-              author_name: `Message from ${req.body.From}`,
-              title: req.body.Body,
-              footer: 'powered by Twilio',
-              footer_icon: 'https://media.licdn.com/mpr/mpr/shrink_200_200/AAEAAQAAAAAAAANCAAAAJGU3Yzg3ODY3LTRlMGEtNGRjNy1iMjIzLWRiZDBjZjU4NjgyYQ.png',
-            },
-          ],
-        }, () => { }
-      );
-    } else {
-      // creates a new channel for new incoming numbers
-      slack.api('channels.create', {
-        name: `sms${usersNumber}`,
-      }, (err, response) => {
-        if (response) {
-          slack.webhook(
-            {
-              channel: `#sms${usersNumber}`,
-              icon_emoji: ':speech_balloon:',
-              username: process.env.SLACK_BOT_NAME,
-              attachments: [
-                {
-                  fallback: `from ${req.body.From}: ${req.body.Body}`,
-                  color: '#3D91FC',
-                  author_name: `Message from ${req.body.From}`,
-                  title: req.body.Body,
-                  footer: 'powered by Twilio',
-                  footer_icon: 'https://media.licdn.com/mpr/mpr/shrink_200_200/AAEAAQAAAAAAAANCAAAAJGU3Yzg3ODY3LTRlMGEtNGRjNy1iMjIzLWRiZDBjZjU4NjgyYQ.png',
-                },
-              ],
-            }, () => { }
-          );
+        // continues conversations in same channels, if they exist
+        if (validChannel(`sms${usersNumber}`)) {
+            slack.webhook({
+                channel: `#sms${usersNumber}`,
+                icon_emoji: ':speech_balloon:',
+                username: process.env.SLACK_BOT_NAME,
+                attachments: [{
+                    fallback: `from ${req.body.From}: ${req.body.Body}`,
+                    color: '#3D91FC',
+                    author_name: `Message from ${req.body.From}`,
+                    title: req.body.Body,
+                    footer: 'powered by Graham Digital',
+                    footer_icon: 'http://asimpson.s3.amazonaws.com/gdlogo.png',
+                }, ],
+            }, () => {});
+        } else {
+            // creates a new channel for new incoming numbers
+            slack.api('channels.create', {
+                name: `sms${usersNumber}`,
+            }, (err, response) => {
+                if (response) {
+                    slack.webhook({
+                        channel: `#sms${usersNumber}`,
+                        icon_emoji: ':speech_balloon:',
+                        username: process.env.SLACK_BOT_NAME,
+                        attachments: [{
+                            fallback: `from ${req.body.From}: ${req.body.Body}`,
+                            color: '#3D91FC',
+                            author_name: `Message from ${req.body.From}`,
+                            title: req.body.Body,
+                            footer: 'powered by Twilio',
+                            footer_icon: 'https://media.licdn.com/mpr/mpr/shrink_200_200/AAEAAQAAAAAAAANCAAAAJGU3Yzg3ODY3LTRlMGEtNGRjNy1iMjIzLWRiZDBjZjU4NjgyYQ.png',
+                        }, ],
+                    }, () => {});
+                }
+            });
         }
-      });
-    }
 
-    // Send back Twilio an empty XML response to let them know we got the message
-    res.writeHead(200, { 'Content-Type': 'text/xml' });
-    const twiml = [{ Response: '' }];
-    res.end(xml(twiml));
-  },
+        // Send back Twilio an empty XML response to let them know we got the message
+        res.writeHead(200, { 'Content-Type': 'text/xml' });
+        const twiml = [{ Response: '' }];
+        res.end(xml(twiml));
+    },
 };
